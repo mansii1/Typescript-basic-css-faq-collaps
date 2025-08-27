ef6fb527903b586bfda773541d688982ab1ed6c3
mansii1 <mansimangukiya0@gmail.com>
2025-08-27 21:55:49 +0530

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const toggles = document.querySelectorAll<HTMLButtonElement>(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    const parent = toggle.parentElement;
+    if (parent) {
+      parent.classList.toggle("active");
+    }
+  });
+});
